c5800cc6e91b5ac783aaa218c8667e53e87c1e8d
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2026-01-11 14:15:09 +0530

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,1 +1,16 @@
+export default function handler(req, res) {
+  if (!globalThis.visitors) {
+    globalThis.visitors = [];
+  }
 
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
+    req.socket?.remoteAddress ||
+    "unknown";
+
+  const time = new Date().toLocaleString();
+
+  globalThis.visitors.push({ ip, time });
+
+  res.status(200).json({ success: true });
+}
