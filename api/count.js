869fdbbfb66ddf923fe79a34d825204bb463aa10
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2026-01-14 00:29:18 +0530

    Update count.js

--- api/count.js
+++ api/count.js
@@ -14,7 +14,9 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
+
     res.status(200).json({ total: data.length });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
