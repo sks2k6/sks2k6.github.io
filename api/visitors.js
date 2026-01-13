bf0f66f15be282611493e1414f3cb1dae788a41d
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2026-01-14 01:21:41 +0530

    Update visitors.js

--- api/visitors.js
+++ api/visitors.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `${SUPABASE_URL}/rest/v1/visitors?select=*`,
+      `${SUPABASE_URL}/rest/v1/visitors?select=*&order=created_at.desc`,
       {
         headers: {
           apikey: SUPABASE_KEY,
@@ -14,9 +14,8 @@ export default async function handler(req, res) {
       }
     );
 
-   const data = await response.json();
-res.status(200).json(data);
-
+    const data = await response.json();
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
